3ec0686bbf372005d80b756c6a86df96ae2b8996
Kajal Kumari <kkumari00571@gmail.com>
2026-06-12 18:32:24 +0530

    Implement hybrid inheritance with Animal and Pet
    Added hybrid inheritance example with Animal, Dog, and Pet classes.

--- Inheritance/HybridInheritance.java
+++ Inheritance/HybridInheritance.java
@@ -1,35 +1,43 @@
 // Demonstrate the working of hybrid inheritance in Java.
 
-class C {    
-  public void disp()    
-  {    
-    System.out.println("C");    
-  }    
-}    
-class A extends C  {    
-  public void disp()    
-  {    
-    System.out.println("A");    
-  }    
-}    
-class B extends C  {    
-  public void disp()    
-  {    
-    System.out.println("B");    
-  }       
-}    
-public class D extends A  {    
-  public void disp()    
-  {    
-    System.out.println("D");    
-  }    
-  public static void main(String args[]) {    
-  D obj = new D();    
-  obj.disp();    
-  }    
-}    
+// Base class
+class Animal {
+    void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+// Derived class (Single Inheritance)
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Another base interface
+interface Pet {
+    void play();
+}
+
+// Hybrid Inheritance: Dog inherits Animal and implements Pet
+class Puppy extends Dog implements Pet {
+    public void play() {
+        System.out.println("Puppy loves to play");
+    }
+}
+
+public class HybridInheritanceDemo {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+        p.eat();   // from Animal
+        p.bark();  // from Dog
+        p.play();  // from Pet
+    }
+}
 
 
-// Output:
+// Output
 
-// D
+// Animal eats food
+// Dog barks
+// Puppy loves to play
